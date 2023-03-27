76c92adbd66fd478b5a53e33cae0a5ccefe63b5a
ngodingasik99 <ngodingasik7@gmail.com>
2023-03-27 14:49:46 +0700

    menghitung harga berdasarkan jumlah barang

--- resources/views/keranjang/index.blade.php
+++ resources/views/keranjang/index.blade.php
@@ -20,72 +20,38 @@
                 </thead>
                 @php $p=1 @endphp
                 @foreach ($data as $item)
-                {{-- @for ($a = 0; $a < $data ; $a++) --}}
                 <tbody>
                   <tr>
                     <th scope="row">{{$p++}}</th>
                     <td>{{$item->namaproduk}}</td>
-                    <td>{{$item->harga}}</td>
                     <td>
-
-                        <form action="/keranjang/{{$item->id}}" method="post">
-                            @method('put')
-                            @csrf
-                            {{-- <input type="hidden" > --}}
-                            <input type="text" value="{{$item->jumlahbarang}}" name="jumlahbarang">
-                            <button type="submit" class="btn btn-success">Hitung</button>
-                        </form>
+                      <input type="text" id="harga{{$p}}" value="{{$item->harga}}" name="harga">
+                    </td> 
+                    <td>
+                      <input type="text" id="jumlahbarang{{$p}}" name="jumlahbarang" onkeyup="InputSub({{$p}});">
                     </td>
-                    <td><input type="text"></td>
+                    <td>
+                      <input type="text" id="subtutal{{$p}}" name="subtotal" onkeyup="InputSub();" readonly>
+                    </td>
                     <td>{{$item->deskripsi}}</td>
-                    {{-- <td>
-                      <a href="/produk/{{$item->id}}" class="btn btn-danger">Delete</a>
-                      <a href="/produk/{{$item->id}}/update" class="btn btn-success">Update</a>
-                      <a href="/produk/{{$item->id}}" class="btn btn-warning">Masukkan Keranjang</a>
-                    </td> --}}
                   </tr>
                 </tbody>
                 @endforeach
-                {{-- @endfor --}}
               </table>
             </div>
           </div>
-        {{-- <div class="col-md-3">
-          <style>
-            .pho {
-                position: relative;
-                height: 400px;
-                width: 300px;
-                overflow: auto;
-              }
-            .d {
-                display: block;
-              }
-          </style>
-          <div class="bungkus">
-            <div class="card-header">
-              <h4 style="color: white; text-align:center;">Keranjang</h4>
-            </div>
-            <div class="d pho">
-              <table class="table table-bordered table-striped mb-0">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title">Nama Produk</h5>
-                    <p class="card-text">harga 
-                      <p>jumlahbarang</p> 
-                      <p>subtotal</p> 
-                      <p>deskripsi</p>  
-                    </p>
-                  </div>
-                </div>
-              </table>
-            </div><br>
-            <div class="card" style="width: 18rem;">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Total</li>
-              </ul>
-            </div>
-          </div>
-        </div> --}}
 </div>
+
+<script>
+  function InputSub(number_id) {
+    console.log(number_id);
+        // var save = $data
+        var harga =  parseInt(document.getElementById('harga'+number_id).value);
+        var jumlahbarang =  parseInt(document.getElementById('jumlahbarang'+number_id).value);
+        var hasil = parseInt(harga) * parseInt(jumlahbarang);
+        if (!isNaN(hasil)) {
+          document.getElementById('subtutal'+number_id).value = hasil;
+        };
+      };
+</script>
 @endsection
