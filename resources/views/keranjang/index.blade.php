5f62644b68dd9fc9750862622aef71bfc2db3b80
ngodingasik99 <ngodingasik7@gmail.com>
2023-03-28 11:06:19 +0700

    mengubah tampilan views keranjang

--- resources/views/keranjang/index.blade.php
+++ resources/views/keranjang/index.blade.php
@@ -1,6 +1,33 @@
 @extends('layouts.app')
 @section('content')
-<div class="container"><br>
+<section>
+  <div class="container"> 
+    <div class="" style="text-align: center"><span class="badge bg-primary      rounded-pill"><input type="number" id="total" name="totalsemua" onkeyup="InputSub();" readonly></span>
+    </div><br><br><br> 
+    <div class="row justify-content-md-center g-5">
+      <?php $p=1; ?>
+      @foreach ($data as $item)
+      <?php $p++ ?>
+      <div class="col-md-3">
+        <div class="card" style="width: 18rem;">
+          <img src="{{asset('/foto.jpg')}}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{$item->namaproduk}}</h5>
+            <p class="card-text">{{$item->deskripsi}}</p>
+            <ul class="list-group">
+              <li class="list-group-item">Harag :<input type="text" id="harga{{$p}}" value="{{$item->harga}}" name="harga" readonly></li>
+              <li class="list-group-item">Jumlah Barang :<input type="text" id="jumlahbarang{{$p}}" name="jumlahbarang" onkeyup="InputSub({{$p}});"></li>
+              <li class="list-group-item">Subtotal :<input type="text" id="subtutal{{$p}}" name="subtotal" onkeyup="InputSub();" readonlyp></li>
+            </ul>
+            {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </div>
+</section><br><br><br><br><br>
+{{-- <div class="container"><br>
           <div class="card">
             <div class="card-header">
                 <h1 class="" style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif">Keranjang</h1>
@@ -41,7 +68,7 @@
               <input type="number" id="total" name="totalsemua" onkeyup="InputSub();" readonly>
             </div>
           </div>
-</div>
+</div> --}}
 <script>
   function InputSub(number_id) {
         var harga =  parseInt(document.getElementById('harga'+number_id).value);
